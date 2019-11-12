d6c4177287d9dbd07d8e1c1cd474395d75d1c8e6
Adrián Herrera Darias <alu0100799101@ull.edu.es>
2019-11-12 23:40:41 +0000

    Inicializada la clase alimento para las pruebas con sus variables

--- lib/Alimentos.rb
+++ lib/Alimentos.rb
@@ -1,5 +1,13 @@
-require "Alimentos/version"
-
 module Alimentos
-  # Your code goes here...
+	 Class Alimento 
+		attr_reader :nombre, :proteinas , :carbohidratos , :lipidos, :gei , :terreno
+		
+		def initialize(nombre,proteinas,carbohidratos,lipidos,gei,terreno)
+		@nombre=@nombre
+		@proteinas=@proteinas
+		@carbohidratos=@carbohidratos
+		@lipidos=@lipidos
+		@gei=@gei
+		@terreno=@terreno
+	end
 end
