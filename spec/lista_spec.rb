123267a1a19f8fe83f32161004d83ee6cb695155
Adrián Herrera Darias <alu0100799101@ull.edu.es>
2019-11-17 23:05:31 +0000

    Añadida la prueba para los nodos

---
+++ spec/lista_spec.rb
@@ -0,0 +1,15 @@
+equire 'spec_helper'
+require  'benchmark'
+    RSpec.describe Alimentos::Nodo do
+        before :all do
+            @mi_nodo = Alimentos::Nodo.new(2,nil,nil)
+        end
+    
+        describe "Atributos del nodo" do
+            it "El nodo contiene los atributos datos, siguiente y previo" do
+                expect(@mi_nodo).to have_attributes(:valor=>2)
+                expect(@mi_nodo).to have_attributes(:siguiente=>nil)
+                expect(@mi_nodo).to have_attributes(:previo=>nil)
+            end
+        end
+    end 
