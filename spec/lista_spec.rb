1fff3418670a873b6f5f680651e305aa794812ef
Adrián Herrera Darias <alu0100799101@ull.edu.es>
2019-11-18 20:24:50 +0000

    Prueba para la lista , se extrae por el principio y el final

--- spec/lista_spec.rb
+++ spec/lista_spec.rb
@@ -38,6 +38,18 @@ require 'spec_helper'
 		            @mi_lista.push_front('Pepe')
 		            expect(@mi_lista.front).to eq('Pepe')
 			end
+			it "Se puede extraer un elemento  al principio de la lista y al final de la lista " do
+				@mi_lista.push_back(1003)
+				@mi_lista.push_front('Pepe')
+				expect(@mi_lista.n_nodos).to eq(2)
+				expect(@mi_lista).to respond_to (:pop_back)
+				expect(@mi_lista.pop_back).to eq(1003)
+				expect(@mi_lista.n_nodos).to eq(1)
+				expect(@mi_lista).to respond_to(:pop_front)
+				expect(@mi_lista.pop_front).to eq('Pepe')
+				expect(@mi_lista.n_nodos).to eq(0)
+
+			end
                 end
 
 
