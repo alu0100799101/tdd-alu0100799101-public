bf4b55777c7f314b988acbc59e246e213ddf1600
Adrián Herrera Darias <alu0100799101@ull.edu.es>
2019-11-25 19:19:55 +0000

    Posibilidades de codigo comentadas debido a que no tengo claridad

--- lib/Alimentos/platos.rb
+++ lib/Alimentos/platos.rb
@@ -2,9 +2,8 @@
 
 module Alimentos
 	class Platos
-		def initialize(nombre,conjuntoalimentos,conjuntogramos)
-		
-		end
+		attr_reader :nombre,:conjuntoalimentos,:conjuntogramos
+		#conjutoalimentos = [Alimentos::Alimento.new("Carne de Vaca",21.1,0.0,3.1,50.0,164.0),Alimentos::Alimento.new("Carne de Cordero",18.0,0.0,17.0,20.0,185.0)]
 	
 	end
 end
--- spec/platos_spec.rb
+++ spec/platos_spec.rb
@@ -2,7 +2,7 @@ require'spec_helper.rb'
 
 RSpec.describe Alimentos::Platos do
 		before  :each do
-		@platos = Alimentos::Platos.new(Alimentos::Lista.new(Alimentos::Alimento.new("Carne de Vaca",21.1,0.0,3.1,50.0,164.0))
+		#@platos = Alimentos::Platos.new(Alimentos::Lista.new(Alimentos::Alimento.new("Carne de Vaca",21.1,0.0,3.1,50.0,164.0))
 		end
 
 
