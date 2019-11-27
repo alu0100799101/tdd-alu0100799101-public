45a0231a5aa2791ded79a3f59a0c8da90b5d9e43
Adrián Herrera Darias <alu0100799101@ull.edu.es>
2019-11-27 18:07:39 +0000

    Prueba de que existe y tiene nombre el plato con exito

--- spec/platos_spec.rb
+++ spec/platos_spec.rb
@@ -2,13 +2,19 @@ require'spec_helper.rb'
 
 RSpec.describe Alimentos::PlatosN do
 		before  :each do
-		@alimento1 = Alimentos::Alimento.new("Carne de Vaca",21.1,0.0,3.1,50.0,164.0)
-		@alimento2 = Alimentos::Alimento.new("Carne de Cordero",18.0,0.0,17.0,20.0,185.0)
-		@alimento3 = Alimentos::Alimento.new("Camarones",17.6,1.5,0.6,18.0,2.0)
-		@la = Alimentos::Lista.new(@alimento1,@alimento2,@alimento3)
-		@lg = Alimentos::Lista.new(24.2,35,19.7)
+		@la = Alimentos::Lista.new()
+		@lg = Alimentos::Lista.new()
 		@plato1 = Alimentos::PlatosN.new("Primer Plato",@la,@lg)
 		end
+	describe ' Pruebas para PlatoNutricional' do
+		it 'Pruebas para el nombre del plato' do
+			expect(@plato1.nombre).to eq("Primer Plato")
+			expect(@plato1).to have_attributes(:nombre => "Primer Plato")
+		
+		end
+	
+	
+	end
 
 
 
