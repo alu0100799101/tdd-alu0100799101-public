f9bb70306f0c254894766da902a47e35061fd1cf
Adrián Herrera Darias <alu0100799101@ull.edu.es>
2019-11-27 18:35:24 +0000

    Existe un conjunto de alimentos

--- spec/platos_spec.rb
+++ spec/platos_spec.rb
@@ -4,12 +4,23 @@ RSpec.describe Alimentos::PlatosN do
 		before  :each do
 		@la = Alimentos::Lista.new()
 		@lg = Alimentos::Lista.new()
+		@alimento1 = Alimentos::Alimento.new("Carne de Vaca",21.1,0.0,3.1,50.0,164.0)
+		@alimento2 = Alimentos::Alimento.new("Carne de Cordero",18.0,0.0,17.0,20.0,185.0)
+		@alimento3 = Alimentos::Alimento.new("Camarones",17.6,1.5,0.6,18.0,2.0)
 		@plato1 = Alimentos::PlatosN.new("Primer Plato",@la,@lg)
+		@la.push_back([@alimento1,@alimento2,@alimento3])
 		end
 	describe ' Pruebas para PlatoNutricional' do
 		it 'Pruebas para el nombre del plato' do
 			expect(@plato1.nombre).to eq("Primer Plato")
 			expect(@plato1).to have_attributes(:nombre => "Primer Plato")
+		
+		end
+		it ' Prueba Existe conjunto de alimentos ' do
+			
+			expect(@plato1).to respond_to(:la)
+            
+
 		
 		end
 	
