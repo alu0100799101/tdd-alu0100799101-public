8d41767c4e1fcbf25d908ed7ff5d98392d388036
Adrián Herrera Darias <alu0100799101@ull.edu.es>
2019-11-27 18:00:44 +0000

    Algunas pruebas para ver como funcione el module y el codigo fuente para que funcione prueba.rspec

--- lib/Alimentos/platos.rb
+++ lib/Alimentos/platos.rb
@@ -1,9 +1,13 @@
 
 
 module Alimentos
-	class Platos
-		attr_reader :nombre,:conjuntoalimentos,:conjuntogramos
-		#conjutoalimentos = [Alimentos::Alimento.new("Carne de Vaca",21.1,0.0,3.1,50.0,164.0),Alimentos::Alimento.new("Carne de Cordero",18.0,0.0,17.0,20.0,185.0)]
+	class PlatosN
+		attr_reader :nombre,:la,:lg
+		def initialize(nombre , la ,lg)
+		@nombre = nombre
+		@lg = lg
+		@la = la
+		end
 	
 	end
 end
