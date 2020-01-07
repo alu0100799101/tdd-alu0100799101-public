eb2d71acb181a32a3822f9822580cc5b652ccaab
Adrián Herrera Darias <alu0100799101@ull.edu.es>
2020-01-07 20:18:38 +0000

    Metodo de inicialización y block introducido

--- lib/Alimentos/menu.rb
+++ lib/Alimentos/menu.rb
@@ -5,9 +5,21 @@
 
 module Alimentos
 	Class Menu
-		attr_accesor : descripcion,precio 
+		attr_accessor	:descripcion,:precio
+	
+	  def initialize(name, &block)
+	    @name = name
+	    @precio = []
+	    
 
-
+		if block_given?  
+		      if block.arity == 1
+			yield self
+			      else
+			       instance_eval(&block) 
+			      end
+			end
+	  	end
 
 
 	end
